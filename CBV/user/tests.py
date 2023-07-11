4bd05b8b1fc6f10a9ce7a744d977469c0eaa145c
houxiaoqiu@163.com <houxiaoqiu@163.com>
2023-07-11 17:15:35 +0800

    cbv

--- CBV/user/tests.py
+++ CBV/user/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+# class B(object):
+#     def foo(self):
+#         print("B foo")
+# class Animal(B):
+#     name="xxx"
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#     def foo(self):
+#         print("A foo")
+
+
+# alex = Animal("alex",23)
+# print(alex.name)
--- CBV/user/views.py
+++ CBV/user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views import View
 
 # Create your views here.
+
+# FBV
+def login(request):
+    if request.method == "GET":
+        return HttpResponse("GET...")
+    else:
+        return HttpResponse("POST...")
+    
+# CBV
+class LoginView(View):
+    def get(self,request):
+        return HttpResponse("LoginView GET...")
+    
+    def post(self,request):
+        return HttpResponse("LoginView POST...")
