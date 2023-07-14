93f6e521b916e8d01a1a45adfdf41cbbf6a55d1e
houxiaoqiu@163.com <houxiaoqiu@163.com>
2023-07-14 15:40:09 +0800

    vue Home

--- frontends/src/App.vue
+++ frontends/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="row">
-    <Lyb/>
+  <div>
+    <Home/>
+    <div  class="row">
+      <Lyb/>
+    </div>
   </div>
 </template>
 
 <script>
   import Lyb from './components/Lyb.vue'
+  import Home from './components/Home.vue'
 
   export default {
     name: 'App',
     components: {
+      Home,
       Lyb
     }
   }
